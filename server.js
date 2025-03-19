103d2d21dbee011d1b8cca4b771748d503c85eff
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-03-19 05:49:32 +0500

    Multer qoshildi

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config()
+const app = express();
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server started has been on PORT: ${PORT}`)
+});
